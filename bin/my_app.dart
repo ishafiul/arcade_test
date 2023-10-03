0ebcaecc4bf6e9931ac0496a9b1546e59c1acaaa
ishaf <shafiulislam20@gmail.com>
2023-10-03 21:38:58 +0600

    port

--- bin/my_app.dart
+++ bin/my_app.dart
@@ -3,7 +3,7 @@ import 'package:my_app/injector.dart';
 
 Future<void> main() {
   // We start the server on port 8080.
-  return runServer(port: 8080, init: () {
+  return runServer(port: 3001, init: () {
     // We define our routes here. The first argument is the path and the second is the handler.
     Route.get('/hello').handle((RequestContext context) => 'Hello World!');
   });
